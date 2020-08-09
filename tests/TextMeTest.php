8f0a52c8e6aa176878d3f05de7adec6bb83011be
steph bug <plexikon@protonmail.com>
2020-08-09 18:20:05 +0200

    add tests

--- tests/TextMeTest.php
+++ tests/TextMeTest.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace Plexikon\Actions\Tests;
 
+use Generator;
 use PHPUnit\Framework\TestCase;
 use Plexikon\Actions\Commons\TextMe;
 
@@ -16,5 +17,24 @@ final class TextMeTest extends TestCase
         $instance = new TextMe('foo');
 
         $this->assertEquals('foo', $instance->text());
+    }
+
+    /**
+     * @test
+     * @dataProvider provideString
+     * @param string $text
+     */
+    public function it_return_text_all_caps(string $text): void
+    {
+        $instance = new TextMe($text);
+
+        $this->assertEquals('FOO_BAR', $instance->format());
+    }
+
+    public function provideString(): Generator
+    {
+        yield ['foo_bar'];
+        yield ['Foo_Bar'];
+        yield ['foo_BAR'];
     }
 }
