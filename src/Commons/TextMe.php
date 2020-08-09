21757bc69f2a716e6e792d90660ded47371d0990
steph bug <plexikon@protonmail.com>
2020-08-09 17:48:03 +0200

    correct text class

---
+++ src/Commons/TextMe.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types=1);
+
+namespace Plexikon\Actions\Commons;
+
+final class TextMe
+{
+    private string $text;
+
+    public function __construct(string $text)
+    {
+        $this->text = $text;
+    }
+
+    public function text(): string
+    {
+        return $this->text;
+    }
+}
